6f4b9b36556230c2d7ec296015395fc92059e57d
Artem <cartem33@gmail.com>
2025-11-28 02:34:32 +0300

    3. Каркас шахматной доски. Class Logic and Test on Logic

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,24 +1,42 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
-    public void whenMoveThenFigureNotFoundException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenOccupiedCellException() {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.F8));
+        logic.add(new PawnBlack(Cell.E7));
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.F8, Cell.B4);
+        });
+        assertThat(exception.getMessage()).isEqualTo("The cell is occupied");
+    }
+
+    @Test
+    public void whenFigureNotFoundException() {
         Logic logic = new Logic();
         FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
-//                ru.job4j.chess.firuges.Cell;
+    }
+
+    @Test
+    public void whenImpossibleMoveException() {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.F8));
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.F8, Cell.F5);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from F8 to F5");
     }
 }
